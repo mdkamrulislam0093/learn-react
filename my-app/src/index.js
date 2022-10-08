0cf40871b68ef39cdd5fe8d63a85308e7b1e90d7
kamrul-home <kamrul-home@kamrul.pc>
2022-10-08 23:48:24 +0600

    add my-app home

---
+++ my-app/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+export default function App(){
+
+  const [isImportant, setIsImportant] = React.useState('Yes');
+
+  function handleClick () {
+    setIsImportant('No');
+  }
+
+return (
+  <div className='state'>
+    <h1 className='state--title'>Is state important to know?</h1>
+    <div className='state--value' onClick={handleClick}>
+      <h1>{isImportant}</h1>
+    </div>
+  </div> 
+)
+
+}
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
