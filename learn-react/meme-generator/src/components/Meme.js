ffb903e35912642d160f7fd1b6de238f780f225f
kamrul <kamrul@kamrul.pc>
2022-10-15 14:45:53 +0600

    15 OCT Office

--- learn-react/meme-generator/src/components/Meme.js
+++ learn-react/meme-generator/src/components/Meme.js
@@ -3,14 +3,25 @@ import memesData from "./data/get_memes";
 
 export default function Meme(){
 
-    let [url, setgenerateimgUrl] = React.useState('');
+    // let [url, setgenerateimgUrl] = React.useState('https://i.imgflip.com/30b1gx.jpg');
+
+    const [ meme, setmeme ] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'https://i.imgflip.com/30b1gx.jpg'
+    });
+
+    const [ allMemeImage, setallMemeImage ] = React.useState(memesData);
 
     function generateImage(e){
         e.preventDefault();
-        if( memesData.success ) {
-            const memesArr = memesData.data.memes;
+        if( allMemeImage.success ) {
+            const memesArr = allMemeImage.data.memes;
             const RandomNumer = Math.floor(Math.random() * memesArr.length);
-            setgenerateimgUrl(memesArr[RandomNumer].url);
+            setmeme(prevmeme => ({
+                ...meme,
+                randomImage: memesArr[RandomNumer].url
+            }));
         }
     }
 
@@ -22,7 +33,7 @@ export default function Meme(){
                 <input type="text" placeholder="Bottom Text" className="bottom_text"/>
                 <button onClick={generateImage}>Get a new meme image</button>
             </form>
-            <img src={url} />
+            <img src={meme.randomImage} />
         </main>
     );
 }
--- learn-react/my-app/src/index.js
+++ learn-react/my-app/src/index.js
@@ -21,15 +21,82 @@ export default function App(){
   // )
 
 
-  const thingsArray = React.useState(['things 1', 'things 2']);
 
-  thingsArray.map(function(item, value){
-    console.log(item, value);
-  });
+
+
+//   const [thingsArray, setarrayThings] = React.useState(['things 1', 'things 2']);
+
   
-  return(
-    <div>Hello</div>
-  );
+//   function addItem(){
+//     setarrayThings(function(thingssingle){
+//       return [...thingssingle, `things ${thingsArray.length + 1}`]
+//     });
+//   }
+
+// let abc = thingsArray.map((item, value) => <p key={value}>{item}</p> );  
+
+  
+
+
+
+/* Contact Project */
+// let [ contact, setContact ] = React.useState({
+//   firstname: "John",
+//   lastname: "Doe",
+//   phone: "+1 (719) 555-1212",
+//   email: "test@gmail.com",
+//   isFavourite: false
+// });
+  
+
+
+// function toggleFacourite(){
+//   setContact(previousState => {
+//     return { ...previousState, isFavourite: !previousState.isFavourite };
+//   });
+
+// }
+
+//   return(
+//     <div>
+//       <div className='cntainer'>
+//         <img src={'../images/simple-people.png'} className='profile-pic'/>
+//         <img src={contact.isFavourite ? '../images/star-solid.svg' : '../images/star-regular.svg'} className='star' onClick={toggleFacourite} />
+//         <h3>{contact.firstname} {contact.lastname}</h3>
+//         <p className='phone-numer'>{contact.phone}</p>
+//         <p>{contact.email}</p>
+//       </div>
+//     </div>
+//   );
+/* Contact Project */
+
+
+/* Counter */
+
+const [ count, setcount ] = React.useState(0);
+
+function add(){
+  setcount(count + 1);
+}
+
+function subtract(){
+  if ( count > 0 ) {
+    setcount(count - 1);    
+  }
+}
+
+return (
+  <div>
+    <div className='counter'>
+      <button onClick={subtract}>-</button>
+      <h1>{count}</h1>  
+      <button onClick={add}>+</button>
+    </div>
+  </div>
+)
+
+
+/* Counter */
 
 }
 
